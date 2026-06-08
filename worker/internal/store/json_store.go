3bdd7d7c60c46a61a23f628657ddc32d2d031d9d
Artem Komov <komovaar@gmail.com>
2026-06-08 18:43:15 +0300

    feat(worker): add JSON store load and save

---
+++ worker/internal/store/json_store.go
@@ -0,0 +1,53 @@
+package store
+
+import (
+	"encoding/json"
+	"errors"
+	"os"
+	"path/filepath"
+)
+
+const stateFileName = "state.json"
+
+type JSONStore struct {
+	dir string
+}
+
+func NewJSONStore(dir string) *JSONStore {
+	return &JSONStore{dir: dir}
+}
+
+func (s *JSONStore) StatePath() string {
+	return filepath.Join(s.dir, stateFileName)
+}
+
+func (s *JSONStore) Load() (*State, error) {
+	data, err := os.ReadFile(s.StatePath())
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return &State{}, nil
+		}
+
+		return nil, err
+	}
+
+	var state State
+	if err := json.Unmarshal(data, &state); err != nil {
+		return nil, err
+	}
+
+	return &state, nil
+}
+
+func (s *JSONStore) Save(state *State) error {
+	if err := os.MkdirAll(s.dir, 0755); err != nil {
+		return err
+	}
+
+	data, err := json.MarshalIndent(state, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(s.StatePath(), data, 0644)
+}
