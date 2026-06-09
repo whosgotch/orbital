bb244bb619c4343ab7670c3f95691cd212b482de
Artem Komov <komovaar@gmail.com>
2026-06-09 16:25:42 +0300

    feat(worker): wire registry into service

--- worker/internal/app/service.go
+++ worker/internal/app/service.go
@@ -1,11 +1,25 @@
 package app
 
-import "github.com/whosgotch/orbital/worker/internal/store"
+import (
+	"github.com/whosgotch/orbital/worker/internal/agent"
+	"github.com/whosgotch/orbital/worker/internal/store"
+)
 
 type Service struct {
-	store *store.JSONStore
+	store          *store.JSONStore
+	workerRegistry *agent.WorkerRegistry
 }
 
 func NewService(store *store.JSONStore) *Service {
-	return &Service{store: store}
+	return &Service{
+		store:          store,
+		workerRegistry: agent.NewDefaultWorkerRegistry(),
+	}
+}
+
+func NewServiceWithWorkerRegistry(store *store.JSONStore, workerRegistry *agent.WorkerRegistry) *Service {
+	return &Service{
+		store:          store,
+		workerRegistry: workerRegistry,
+	}
 }
